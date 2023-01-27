781d68e180c4e29df4c149002ebc139400e3e872
vzakharenkova <v.s.zakharenkova@gmail.com>
2023-01-27 13:13:44 +0300

    feat: add logic to the restful memberTypes endpoint

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -8,7 +8,9 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 ): Promise<void> => {
   fastify.get('/', async function (request, reply): Promise<
     MemberTypeEntity[]
-  > {});
+  > {
+    return await fastify.db.memberTypes.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +19,19 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const { id } = request.params;
+      const memberType = await fastify.db.memberTypes.findOne({
+        key: 'id',
+        equals: id,
+      });
+
+      if (!memberType) {
+        throw fastify.httpErrors.notFound();
+      }
+
+      return memberType;
+    }
   );
 
   fastify.patch(
@@ -28,7 +42,19 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const { id } = request.params;
+      const memberType = await fastify.db.memberTypes.findOne({
+        key: 'id',
+        equals: id,
+      });
+
+      if (!memberType) {
+        throw fastify.httpErrors.badRequest();
+      }
+
+      return await fastify.db.memberTypes.change(id, request.body);
+    }
   );
 };
 
