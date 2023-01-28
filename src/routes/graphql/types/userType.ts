6a4c3b91efce25ac2f1c4965860e63d92fb7621e
vzakharenkova <v.s.zakharenkova@gmail.com>
2023-01-28 22:18:04 +0300

    feat: get users with their userSubscribedTo, subscribedToUser (nested

--- src/routes/graphql/types/userType.ts
+++ src/routes/graphql/types/userType.ts
@@ -16,25 +16,50 @@ import { memberTypeType } from './memberTypeType';
 import { postType } from './postType';
 import { profileType } from './profileType';
 
+const basicUserFields = {
+  id: { type: GraphQLID },
+  firstName: { type: GraphQLString },
+  lastName: { type: GraphQLString },
+  email: { type: GraphQLString },
+  subscribedToUserIds: { type: new GraphQLList(GraphQLID) },
+};
+
 const basicUserType = new GraphQLObjectType({
   name: 'BasicUser',
-  fields: {
-    id: { type: GraphQLID },
-    firstName: { type: GraphQLString },
-    lastName: { type: GraphQLString },
-    email: { type: GraphQLString },
-    subscribedToUserIds: { type: new GraphQLList(GraphQLID) },
-  },
+  fields: basicUserFields,
 });
+
+const subscriptionInfoFields = {
+  userSubscribedTo: {
+    type: new GraphQLList(basicUserType),
+    resolve: userSubscribedToResolver,
+  },
+  subscribedToUser: {
+    type: new GraphQLList(basicUserType),
+    resolve: subscribedToUserResolver,
+  },
+};
+
+const basicUserWithSubsType = new GraphQLObjectType({
+  name: 'BasicUserWithSubs',
+  fields: { ...basicUserFields, ...subscriptionInfoFields },
+});
+
+const subscriptionInfoNestedFields = {
+  userSubscribedTo: {
+    type: new GraphQLList(basicUserWithSubsType),
+    resolve: userSubscribedToResolver,
+  },
+  subscribedToUser: {
+    type: new GraphQLList(basicUserWithSubsType),
+    resolve: subscribedToUserResolver,
+  },
+};
 
 export const userType = new GraphQLObjectType({
   name: 'User',
   fields: {
-    id: { type: GraphQLID },
-    firstName: { type: GraphQLString },
-    lastName: { type: GraphQLString },
-    email: { type: GraphQLString },
-    subscribedToUserIds: { type: new GraphQLList(GraphQLID) },
+    ...basicUserFields,
     profile: {
       type: profileType,
       resolve: profileByUserIdResolver,
@@ -47,13 +72,6 @@ export const userType = new GraphQLObjectType({
       type: memberTypeType,
       resolve: memberTypeByUserIdResolver,
     },
-    userSubscribedTo: {
-      type: new GraphQLList(basicUserType),
-      resolve: userSubscribedToResolver,
-    },
-    subscribedToUser: {
-      type: new GraphQLList(basicUserType),
-      resolve: subscribedToUserResolver,
-    },
+    ...subscriptionInfoNestedFields,
   },
 });
